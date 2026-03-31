8d06bb36bc499c8a4abfac4253664eb296992dce
Victor Nyagudi <nyagudivicktor@gmail.com>
2026-03-31 15:28:12 +0300

    fix(navigation): set shouldGoBack to false
    This prevents the race condition when the confirmation modal is closed and you need to navigate to a different screen

--- src/libs/Navigation/helpers/setNavigationActionToMicrotaskQueue.ts
+++ src/libs/Navigation/helpers/setNavigationActionToMicrotaskQueue.ts
@@ -1,4 +1,11 @@
+import type {WindowState} from '@components/Modal/types';
+
 export default function setNavigationActionToMicrotaskQueue(navigationAction: () => void) {
+    if ((window.history.state as WindowState)?.shouldGoBack) {
+        const currentState = (window.history.state ?? {}) as WindowState;
+        window.history.pushState({...currentState, shouldGoBack: false}, '', null);
+    }
+
     /**
      * The function is used when the app needs to set a navigation action to the microtask queue, it guarantees to execute Onyx.update first, then the navigation action.
      * More details - https://github.com/Expensify/App/issues/37785#issuecomment-1989056726.
